faff2fc62a35d1937bba24f5904174cd6e6bb267
Erickkirui <erickkirui653@gmail.com>
2023-03-14 15:39:17 +0300

    culculate distance in blocks

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupPoint) {
+    //returns the number of blocks given a value
+
+   const headQuater = 42 
+
+   //return pickupPoint - headQuater
+   if (pickupPoint >= 42){
+      
+    return pickupPoint - headQuater
+
+   }
+   else{
+ 
+   return headQuater -pickupPoint  
+   }
+
+
+  }
+
+ distanceFromHqInBlocks(43)
+
